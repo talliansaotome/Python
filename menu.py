290bd3eec5bba991b22f63f084f7ceb5d6c95a67
Ben Miller <tallian.saotome@gmail.com>
2021-07-15 16:19:01 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,23 +1,41 @@
 #!/usr/bin/python3
 ## includes
+import csv
+import subprocess
 
 
-## define static vars such as ssh launch opts
+class VPNTunnel:
 
-## read file into array as menu item,mainopt,opt,opt,... - this stolen code is clearly not enough
+  def __init__(self, line):
+    self.menu_text = line[0]
+    self.ip = line[1]
+    self.user = line[2]  # or wherever it is
+    self.connection_string = f"ssh {self.user}@{self.ip}"
 
-import csv 
+  #def connect(self):
+  #  self.ssh_process = subprocess.run(self.connection_string, shell=True)
 
-items = []
+
+
+servers = []
 
 with open('file.csv') as csvfile:    
-	csvReader = csv.reader(csvfile)    
-	for row in csvReader:        
-		items.append(row[0])        
-print(items)
+  csvReader = csv.reader(csvfile)    
+  for row in csvReader:
+    servers.append(VPNTunnel(row))
 
+##Menu to select choices
+#0 out counter
+i = 0
+#iterate through
+for server in servers:
+  i += 1
+  print(f"{i}): {server.menu_text}")
+  
 
-## create menu from array
+server_choice = input()
 
+#servers[int(server_choice)].connect
+#print(servers[1].connection_string)
 
-## assemble and execute command defined above
+x = subprocess.run(servers[0].connection_string, shell=True)
